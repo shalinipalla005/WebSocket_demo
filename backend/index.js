7112959322d6bbbb427e6d248a9b9c268b4be6e3
Shalini Palla <144434562+shalinipalla005@users.noreply.github.com>
2025-07-28 16:00:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', 1);
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
